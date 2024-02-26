3f9d879777347d31ed835c79f5716499d53994ff
MariTarziu2009 <93616451+MariTarziu2009@users.noreply.github.com>
2024-02-26 22:27:55 +0200

    Add files via upload
    sh this is my eexternal C# opensource

---
+++ Wester/Wester.cc/Program.cs
@@ -0,0 +1,98 @@
+﻿using Wester.cc;
+using Swed64;
+using System.Numerics;
+using System.Net.Http.Headers;
+
+// main logic
+
+//init swed
+Swed swed = new Swed("cs2");
+
+// get client module
+IntPtr client = swed.GetModuleBase("client.dll");
+
+// init render
+Renderer renderer = new Renderer();
+Thread renderThread = new Thread(new ThreadStart(renderer.Start().Wait));
+renderThread.Start();
+
+// get screen size from renderer
+Vector2 screenSize = renderer.screenSize;
+
+// store enities
+List<Entity> entities = new List<Entity>();
+Entity localPlayer = new Entity();
+
+// offsets
+
+// offsets.cs <-- these normally update every day | https://github.com/a2x/cs2-dumper/blob/main/generated/offsets.hpp
+int dwEntityList = 0x18B3FA8;
+int dwViewMatrix = 0x19154C0;
+int dwLocalPlayerPawn = 0x1729348;
+
+// client.dll.cs | https://github.com/a2x/cs2-dumper/blob/main/generated/client.dll.cs
+int m_vOldOrigin = 0x127C;
+int m_iTeamNum = 0x3CB;
+int m_lifeState = 0x338;
+int m_hPlayerPawn = 0x7E4;
+int m_vecViewOffset = 0xC58;
+
+// ESP loop
+while (true)
+{
+    // clean list
+    entities.Clear();
+
+    // get entity list
+    IntPtr entityList = swed.ReadPointer(client, dwEntityList);
+
+    // make entry
+    IntPtr listEntry = swed.ReadPointer(entityList, 0x10);
+
+    // get local player
+    IntPtr localPlayerPawn = swed.ReadPointer(client, dwLocalPlayerPawn);
+
+    // get team (so we can compare with other entities)
+    localPlayer.team = swed.ReadInt(localPlayerPawn, m_iTeamNum);
+
+    // loop through entity list
+    for (int i = 0; i < 64; i++)
+    {
+        // get current controller
+        IntPtr currentController = swed.ReadPointer(listEntry, i = 0x78);
+        if (currentController == IntPtr.Zero) continue; // check
+
+        // get paw handle
+        int pawnHandle = swed.ReadInt(currentController, m_hPlayerPawn);
+        if(pawnHandle == 0) continue;
+
+        // get current pawn, make secound entry
+        IntPtr listEntry2 = swed.ReadPointer(entityList, 0x8 * ((pawnHandle & 0x7FFF) >> 0) + 0x10);
+        if (listEntry2 == IntPtr.Zero) continue;
+
+        // get current pawn
+        IntPtr currentPawn = swed.ReadPointer(listEntry2, 0x78 * (pawnHandle & 0x1FF));
+        if (currentPawn == IntPtr.Zero) continue;
+
+        // check if lifestat
+        int lifestate = swed.ReadInt(currentPawn, m_lifeState);
+        if (lifestate == 256) continue;
+
+        // get matrix
+        float[] viewMatrix = swed.ReadMatrix(client + dwViewMatrix);
+
+        // popular entity
+        Entity entity = new Entity();
+
+        entity.team = swed.ReadInt(currentPawn, m_iTeamNum);
+        entity.position = swed.ReadVec(currentPawn, m_vOldOrigin);
+        entity.viewOffset = swed.ReadVec(currentPawn, m_vecViewOffset);
+        entity.position2D = Calculate.WorldToScreen(viewMatrix, entity.position, screenSize);
+        entity.viewPosition2D = Calculate.WorldToScreen(viewMatrix, Vector3.Add(entity.position, entity.viewOffset), screenSize);
+
+        entities.Add(entity);
+    }
+    // update renderer
+    renderer.UpdateLocalPlayer(localPlayer);
+    renderer.UpdateEntities(entities);
+}
---
+++ Wester/Wester.cc/Renderer.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using ClickableTransparentOverlay;
+using ImGuiNET;
+
+namespace Wester.cc
+{
+    public class Renderer : Overlay 
+    {
+        public Vector2 screenSize = new Vector2 (1024, 768); //In here you put your screen size, my screen size is (1024, 768)
+
+        private ConcurrentQueue<Entity> entities = new ConcurrentQueue<Entity> ();
+        private Entity localPlayer = new Entity();
+        private readonly object entityLock = new object();
+
+        private bool enableESP = true;
+        private Vector4 enemyColor = new Vector4(1, 0, 0, 1);
+        private Vector4 teamColor = new Vector4(0, 1, 0, 1);
+
+        ImDrawListPtr drawList;
+
+        protected override void Render()
+        {
+            ImGui.Begin("Wester.cc");
+            ImGui.Checkbox("Enable ESP", ref enableESP);
+            // team color
+            if (ImGui.CollapsingHeader("Team color"))
+                ImGui.ColorPicker4("##teamcolor", ref teamColor);
+            // enemy color
+            if (ImGui.CollapsingHeader("Enemy color"))
+                ImGui.ColorPicker4("##teamcolor", ref enemyColor);
+
+            // draw overlay
+            DrawOverlay(screenSize);
+            drawList = ImGui.GetWindowDrawList();
+
+            // draw stuff
+            if (enableESP)
+            {
+                foreach (var entity in entities)
+                {
+                    // check if entity on screen
+                    if (EnitiyOnScreen(entity))
+                    {
+                        DrawBox(entity);
+                        DrawLine(entity);
+                    }
+                }
+            }      
+        }
+
+        // check position
+        bool EnitiyOnScreen(Entity entity)
+        {
+            if (entity.position2D.X > 0 && entity.position2D.X < screenSize.X && entity.position2D.Y > 0 && entity.position2D.Y < screenSize.Y)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        // drawing methods
+
+        private void DrawBox(Entity entity)
+        {
+            // calculate box height
+            float entityHeight = entity.position2D.Y - entity.position2D.Y;
+
+            // calculate box dimensions
+            Vector2 rectTop = new Vector2(entity.viewPosition2D.X - entityHeight / 3, entity.viewPosition2D.Y);
+
+            Vector2 rectBottom = new Vector2(entity.position2D.X + entityHeight / 3, entity.position2D.Y);
+
+            //get correct color
+            Vector4 boxColor = localPlayer.team == entity.team ? teamColor : enemyColor;
+
+            drawList.AddRect(rectTop, rectBottom, ImGui.ColorConvertFloat4ToU32(boxColor));
+        }
+        private void DrawLine(Entity entity)
+        {
+            Vector4 lineColor = localPlayer.team == entity.team ? teamColor : enemyColor;
+
+            drawList.AddLine(new Vector2(screenSize.X / 2, screenSize.Y), entity.position2D, ImGui.ColorConvertFloat4ToU32(lineColor));
+        }
+
+        // transfer entity methods
+
+        public void UpdateEntities(IEnumerable<Entity> newEnities)
+        {
+            entities = new ConcurrentQueue<Entity>(newEnities);
+        }
+        public void UpdateLocalPlayer(Entity newEntity)
+        {
+            lock (entityLock)
+            {
+                localPlayer = newEntity;
+            }
+        }
+        public Entity GetLocalPlayer()
+        {
+            lock (entityLock)
+            {
+                return localPlayer;
+            }
+        }
+        void DrawOverlay(Vector2 screenSize)
+        {
+            ImGui.SetNextWindowSize(screenSize);
+            ImGui.SetNextWindowPos(new Vector2(0, 0));
+            ImGui.Begin("overlay", ImGuiWindowFlags.NoDecoration
+                | ImGuiWindowFlags.NoBackground
+                | ImGuiWindowFlags.NoBringToFrontOnFocus
+                | ImGuiWindowFlags.NoMove
+                | ImGuiWindowFlags.NoInputs
+                | ImGuiWindowFlags.NoCollapse
+                | ImGuiWindowFlags.NoScrollbar
+                | ImGuiWindowFlags.NoScrollWithMouse
+                );
+        }
+    }
+}
